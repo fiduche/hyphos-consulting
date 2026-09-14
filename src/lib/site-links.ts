c992a33a386c23e1a0a29cc09e7c783725ab75cd
Daniel Newton <daniel.m.newton@gmail.com>
2026-09-14 10:24:26 -0700

    Refresh Hyphos Inc website and connected software showcase

---
+++ src/lib/site-links.ts
@@ -0,0 +1,2 @@
+export const consultingUrl = import.meta.env.DEV ? 'http://127.0.0.1:4322' : 'https://hyphosconsulting.com';
+export const productUrl = import.meta.env.DEV ? 'http://127.0.0.1:4323' : 'https://hyphos.io';
